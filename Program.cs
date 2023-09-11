676cfbc1a8401dd440747c7585793c319be3a3d6
Kian Dufraimont <339637118@GoTVDSB.ca>
2023-09-11 10:09:10 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Basic_Input
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateTime = DateTime.Now;
+            int year = dateTime.Year;
+            Console.WriteLine("Please provide a name:");
+            string name = Console.ReadLine().Trim();
+            Console.WriteLine("How old are you?");
+            int.TryParse(Console.ReadLine(), out int age);
+            Console.WriteLine($"{name}, you were born in {year - age}");
+
+            //part 2
+
+        }
+    }
+}
